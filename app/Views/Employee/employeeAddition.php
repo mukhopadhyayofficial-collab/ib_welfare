fe5a4aad079c54be0df85ec7b2b788ff0dac0ba6
ibrintudasadhikari <ib.rintudasadhikari@gmail.com>
2026-05-29 14:56:10 +0530

    UI Changes

--- app/Views/Employee/employeeAddition.php
+++ app/Views/Employee/employeeAddition.php
@@ -20,7 +20,7 @@
                 <div class="row g-3 mb-4">
                     <div class="col-md-3"><label class="form-label required">Employee ID </label><input
                             class="form-control" placeholder="EMP001"></div>
-                    <div class="col-md-6"><label class="form-label required">Full Name</label><input
+                    <div class="col-md-6"><label class="form-label required">Full Name</label><input type="text"
                             class="form-control" placeholder="Name of personnel"></div>
                     <div class="col-md-3"><label class="form-label">Rank / Designation</label>
                         <select class="form-select">
@@ -84,8 +84,8 @@
                         </select></div>
                     <div class="col-md-3"><label class="form-label">Date of Birth</label><input type="date" id="dob"
                             class="form-control"></div>
-                    <div class="col-md-3"><label class="form-label">Age</label><input id="age" class="form-control"
-                            placeholder="Enter age"></div>
+                    <div class="col-md-3"><label class="form-label">Age</label><input id="age" class="form-control" readonly>
+                    </div>
                     <div class="col-md-3"><label class="form-label">Date of Joining</label><input type="date"
                             class="form-control"></div>
                     <div class="col-md-3"><label class="form-label">Service Status</label><select class="form-select">
@@ -94,8 +94,15 @@
                             <option>Transferred</option>
                             <option>On Leave</option>
                         </select></div>
-                    <div class="col-md-3"><label class="form-label">Basic Pay Allowance</label><input
-                            class="form-control" type="number"></div>
+                    <div class="col-md-3"><label class="form-label">Basic Pay Allowance</label>
+                    <input
+                            type="text"
+                            name="basic_pay"
+                            class="form-control"
+                            placeholder="e.g. 45000.50"
+                            pattern="^\d+(\.\d{1,2})?$"
+                            oninput="this.value=this.value.replace(/[^0-9.]/g,'').replace(/(\..*)\./g,'$1');">
+                    </div>
                     <div class="col-md-3"><label class="form-label">Photo</label><input type="file" class="form-control" accept="image/*"></div>
 
                 </div>
@@ -234,16 +241,26 @@
                             <option>South 24 Parganas</option>
                             <option>Uttar Dinajpur</option>
                         </select></div>
-                    <div class="col-md-3"><label class="form-label required">PIN Code</label><input id="presentPin"
-                            class="form-control address-present" placeholder="PIN Code" maxlength="6"></div>
+                    <div class="col-md-3"><label class="form-label required">PIN Code</label>
+                    <input
+                            type="text"
+                            id="presentPin"
+                            name="presentPin"
+                            class="form-control address-present"
+                            placeholder="PIN Code"
+                            maxlength="6"
+                            pattern="[0-9]{6}"
+                            required
+                            oninput="this.value=this.value.replace(/[^0-9]/g,'').slice(0,6)">
+                    </div>
                     <div class="col-12 mt-2">
                         <div class="address-band"><strong><i class="bi bi-house-check"></i> Permanent Address</strong>
                         </div>
                     </div>
-                    <div class="col-md-3"><label class="form-label required">City / Village</label><input
+                    <div class="col-md-3"><label class="form-label required">Address Line1/ Village</label><input
                             id="permanentCity" class="form-control address-permanent" placeholder="City / Village">
                     </div>
-                    <div class="col-md-3"><label class="form-label required">Post Office</label><select id="permanentPO"
+                    <div class="col-md-3"><label class="form-label required">Address Line2/ Post Office</label><select id="permanentPO"
                             class="form-select address-permanent">
                             <option value="">Select Post Office</option>
                             <option>Kolkata GPO</option>
@@ -259,6 +276,24 @@
                             <option>Midnapore</option>
                             <option>Purulia</option>
                         </select></div>
+
+
+                    <div class="col-md-3"><label class="form-label required">Address Line3/ City/Town</label><select id="presentPO"
+                            class="form-select address-present">
+                            <option value="">Select City/Town</option>
+                            <option>Kolkata GPO</option>
+                            <option>Bhowanipore</option>
+                            <option>Alipore</option>
+                            <option>Barasat</option>
+                            <option>Krishnanagar</option>
+                            <option>Berhampore</option>
+                            <option>Asansol</option>
+                            <option>Durgapur</option>
+                            <option>Siliguri</option>
+                            <option>Malda</option>
+                            <option>Midnapore</option>
+                            <option>Purulia</option>
+                        </select></div>    
                     <div class="col-md-3"><label class="form-label required">Police Station</label><select
                             id="permanentPS" class="form-select address-permanent">
                             <option value="">Select Police Station</option>
@@ -302,8 +337,18 @@
                             <option>South 24 Parganas</option>
                             <option>Uttar Dinajpur</option>
                         </select></div>
-                    <div class="col-md-3"><label class="form-label required">PIN Code</label><input id="permanentPin"
-                            class="form-control address-permanent" placeholder="PIN Code" maxlength="6"></div>
+                    <div class="col-md-3"><label class="form-label required">PIN Code</label>
+                    <input
+                            type="text"
+                            id="presentPin"
+                            name="presentPin"
+                            class="form-control address-present"
+                            placeholder="PIN Code"
+                            maxlength="6"
+                            pattern="[0-9]{6}"
+                            required
+                            oninput="this.value=this.value.replace(/[^0-9]/g,'').slice(0,6)">
+                    </div>
                 </div>
 
                 <div class="section-title"><i class="bi bi-heart-pulse"></i> Basic Health Profile</div>
@@ -319,10 +364,26 @@
                             <option>O+</option>
                             <option>O-</option>
                         </select></div>
-                    <div class="col-md-3"><label class="form-label">Height (cm)</label><input id="heightCm"
-                            type="number" class="form-control" placeholder="e.g. 170"></div>
-                    <div class="col-md-3"><label class="form-label">Weight (kg)</label><input id="weightKg"
-                            type="number" class="form-control" placeholder="e.g. 70"></div>
+                    
+                    <div class="col-md-3">
+                        <label class="form-label">Height (cm)</label>
+                        <input
+                            id="heightCm"
+                            type="text"
+                            class="form-control"
+                            placeholder="e.g. 170"
+                            maxlength="3"
+                            oninput="this.value=this.value.replace(/[^0-9]/g,'').slice(0,3)">
+                    </div>
+                    <div class="col-md-3"><label class="form-label">Weight (kg)</label>
+                    <input
+                            id="weightKg"
+                            type="text"
+                            class="form-control"
+                            placeholder="e.g. 70"
+                            maxlength="3"
+                            oninput="this.value=this.value.replace(/[^0-9]/g,'').slice(0,3)">
+                    </div>
                 </div>
 
                 <div class="section-title d-flex justify-content-between align-items-center"><span><i
@@ -335,7 +396,7 @@
                             <tr>
                                 <th>Name</th>
                                 <th style="min-width:210px">Relationship</th>
-                                <th>Age</th>
+                                <th>Date of Birth</th>
                                 <th>Mobile</th>
                                 <th>Blood Group</th>
                                 <th>Action</th>
@@ -355,8 +416,19 @@
                                         <option>Brother</option>
                                         <option>Sister</option>
                                     </select></td>
-                                <td><input class="form-control" type="number"></td>
-                                <td><input class="form-control"></td>
+                                <td><input type="date" id="dob" class="form-control"></td>
+                                <td>
+                                    <input
+                                        type="tel"
+                                        name="mobile"
+                                        class="form-control"
+                                        placeholder="Enter 10-digit mobile number"
+                                        maxlength="10"
+                                        pattern="[6-9][0-9]{9}"
+                                        required
+                                        oninput="this.value=this.value.replace(/\D/g,'').slice(0,10)">
+                                
+                                </td>
                                 <td><select class="form-select">
                                         <option value="">Select Blood Group</option>
                                         <option>A+</option>
@@ -377,9 +449,12 @@
 
                 <div class="section-title"><i class="bi bi-journal-medical"></i> Remarks</div>
                 <div class="row g-3">
+                    <div class="col-md-4"><label class="form-label">Known Ailment</label><textarea class="form-control" rows="3">
+                        </textarea></div>
+                    <div class="col-md-4"><label class="form-label">Any Disability / Allergy</label><textarea class="form-control" rows="3"></textarea></div>
 
-                    <div class="col-md-12"><label class="form-label">Corrective Action Taken /
-                            Follow-up</label><textarea class="form-control" rows="2"></textarea></div>
+                    <div class="col-md-4"><label class="form-label">Corrective Action Taken /
+                            Follow-up</label><textarea class="form-control" rows="3"></textarea></div>
                 </div>
                 <div class="mt-4">
                     <button class="btn quick-btn bg-purple requires-super"><i class="bi bi-save"></i> Save Employee
@@ -396,3 +471,21 @@
 </body>
 
 </html>
+<script>
+document.getElementById('dob').addEventListener('change', function () {
+    const dob = new Date(this.value);
+    const today = new Date();
+
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+
+    if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < dob.getDate())
+    ) {
+        age--;
+    }
+
+    document.getElementById('age').value = age;
+});
+</script>
